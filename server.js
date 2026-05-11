8f45fa5f6a29272805cc14773bb75a61e84dfeac
akash <ananiyapradeep4@gmail.com>
2026-05-11 17:33:37 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.static(__dirname));
+
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
