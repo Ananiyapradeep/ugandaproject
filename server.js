996f04723a663179f8d1efc835ba582953e87f02
krisshna12 <krissh1620@gmail.com>
2026-05-16 13:37:04 +0530

    restore api routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
@@ -11,6 +12,17 @@ app.get("/", (req, res) => {
 
 app.get("/admin", (req, res) => {
   res.sendFile(path.join(__dirname, "admin.html"));
+});
+
+app.get("/api/health", (req, res) => {
+  res.json({ status: "Server running" });
+});
+
+app.post("/api/login", (req, res) => {
+  res.json({
+    success: true,
+    token: "demo-admin-token"
+  });
 });
 
 module.exports = app;
