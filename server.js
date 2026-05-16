fa64a58c01b1edc2f534231d994be4cb0ad4ea3a
krisshna12 <krissh1620@gmail.com>
2026-05-16 13:44:22 +0530

    fix cors

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 
 const app = express();
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.static(__dirname));
